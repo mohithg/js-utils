8735250d2ed31f2253523d03129e7b0b67c044a6
Mohith G <mohithgm@gmail.com>
2016-10-31 19:28:23 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+/**
+ * compactObject - deletes the empty valued properties from the object
+ * @params {Object} - The object
+ */
+export const compactObject = (object) => {
+  let clone = _.clone(object);
+  _.each(clone, (value, key) => {
+    /** Not using _.isEmpty since false is also considered empty  */
+    if ( (value === '') || value === undefined || (_.isObject(value) && _.keys(value).length === 0)) {
+      delete clone[key];
+    }
+  });
+  return clone;
+};
+
+/**
+ * recursiveOmit - Removes the empty valued properties from the object recursively
+ * @params {Object} - The object
+ */
+export const recursiveOmit = (object) => {
+  let compactObj = compactObject(object);
+  _.each(compactObj, (value, key) => {
+    if (_.isObject(value)) {
+      compactObj[key] = recursiveOmit(value);
+    }
+    if (_.isObject(compactObj[key]) && _.keys(compactObj[key]).length === 0) {
+      delete compactObj[key];
+    }
+  });
+  return compactObj;
+};
+
+/**
+ * deepExtend - Similar to _.extend but does a deep extend
+ * @params {Object} - The object
+ */
+export function deepExtend(target, source) {
+  if (_.isObject(target) && _.isObject(source)) {
+    _.keys(source).forEach(key => {
+      if (_.isObject(source[key]) || source[key] === null) {
+        if (!target[key]) {
+          _.extend(target, { [key]: {} });
+        }
+        deepExtend(target[key], source[key]);
+      } else {
+        _.extend(target, { [key]: source[key] });
+      }
+    });
+  }
+  return target;
+}
+
+/**
+ * diffObject - Compares Object1 with Object2 and returns the different key-value pairs
+ * @params {Object} - The object1
+ * @params {Object} - The object2
+ * @return {Object} - The diff of object1 and object2
+ */
+export function diffObject(object1, object2) {
+  return Object.keys(object1).concat(Object.keys(object2)).reduce(function (map, key) {
+    if (object1[key] !== object2[key]) {
+      map[key] = object2[key];
+    }
+    return map;
+  }, {});
+}
+
+
+let utils = {
+  compactObject,
+  recursiveOmit,
+  deepExtend,
+  diffObject
+};
+
+export default utils;
