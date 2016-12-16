4c79ca4e9e4baba1177fb0ad10582f814f9d7487
Mohith G <mohithgm@gmail.com>
2016-12-16 12:06:52 +0530

    fixed eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import _ from 'underscore';
+
 /**
  * compactObject - deletes the empty valued properties from the object
  * @params {Object} - The object
  */
 export const compactObject = (object) => {
-  let clone = _.clone(object);
+  const clone = _.clone(object);
   _.each(clone, (value, key) => {
     /** Not using _.isEmpty since false is also considered empty  */
-    if ( (value === '') || value === undefined || (_.isObject(value) && _.keys(value).length === 0)) {
+    if ((value === '') || value === undefined || (_.isObject(value) && _.keys(value).length === 0)) {
       delete clone[key];
     }
   });
@@ -18,7 +20,7 @@ export const compactObject = (object) => {
  * @params {Object} - The object
  */
 export const recursiveOmit = (object) => {
-  let compactObj = compactObject(object);
+  const compactObj = compactObject(object);
   _.each(compactObj, (value, key) => {
     if (_.isObject(value)) {
       compactObj[key] = recursiveOmit(value);
@@ -34,7 +36,7 @@ export const recursiveOmit = (object) => {
  * deepExtend - Similar to _.extend but does a deep extend
  * @params {Object} - The object
  */
-export function deepExtend(target, source) {
+export const deepExtend = (target, source) => {
   if (_.isObject(target) && _.isObject(source)) {
     _.keys(source).forEach(key => {
       if (_.isObject(source[key]) || source[key] === null) {
@@ -48,7 +50,7 @@ export function deepExtend(target, source) {
     });
   }
   return target;
-}
+};
 
 /**
  * diffObject - Compares Object1 with Object2 and returns the different key-value pairs
@@ -56,21 +58,21 @@ export function deepExtend(target, source) {
  * @params {Object} - The object2
  * @return {Object} - The diff of object1 and object2
  */
-export function diffObject(object1, object2) {
-  return Object.keys(object1).concat(Object.keys(object2)).reduce(function (map, key) {
+export const diffObject = (object1, object2) => (
+  Object.keys(object1).concat(Object.keys(object2)).reduce((map, key) => {
     if (object1[key] !== object2[key]) {
-      map[key] = object2[key];
+      map[key] = object2[key]; // eslint-disable-line no-param-reassign
     }
     return map;
-  }, {});
-}
+  }, {})
+);
 
 
-let utils = {
+const utils = {
   compactObject,
   recursiveOmit,
   deepExtend,
-  diffObject
+  diffObject,
 };
 
 export default utils;
